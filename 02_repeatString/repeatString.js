475bf0bb63e76f0ebc87fa60f71291fcce492ffb
oshiogifted <oshiogifted2011@gmail.com>
2022-04-11 21:55:17 -0400

    completed hello world and repeat string exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+
+  let output = '';
+  let err = 'ERROR';
+  if (num < 0) {
+    return err;
+  }
+  for (let i = 0; i < num; i++) {
+    output += str;
+  }
+  return output;
 
 };
 
