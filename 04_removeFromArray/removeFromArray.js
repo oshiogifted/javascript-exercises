4d5355cf456e0347629a7babddaca9c25ccda6ca
oshiogifted <oshiogifted2011@gmail.com>
2022-04-18 21:44:01 -0400

    remove from arry done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr = [], ...removeThis) {
+  return arr.filter(el => !removeThis.includes(el))
 };
 
 // Do not edit below this line
