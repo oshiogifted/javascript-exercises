7ba878145097cfc52936c0d7b30edf1cc8fbc403
oshiogifted <oshiogifted2011@gmail.com>
2022-04-20 16:26:26 -0400

    temp conversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function (num) {
+  // formula to convert f -> c ==> [°C] = ([°F] − 32) × 5⁄9
+  // 32 => 0
+  // toFixed(1) => rounds to decimal place but returns value as string
+  // + => converts string to number
+  // can also use Number(calculation.toFixed(1)) -- Number func
+  return +((num - 32) * 5 / 9).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (num) {
+  // formula to convert c -> f ==> [°F] = [°C] × 9⁄5 + 32
+  // 0 => 32
+  return +((num * 9 / 5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
