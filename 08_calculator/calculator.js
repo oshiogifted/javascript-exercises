0333232e1d65dad50e4a0d6a865a6460f3a36a4c
oshiogifted <oshiogifted2011@gmail.com>
2022-06-08 23:45:52 -0400

    to be continued

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr = []) {
+  let initialVal = 0;
+  // todo
+  return arr.reduce((prevVal, currVal) => {
+    prevVal + currVal
+  }, initialVal)
+
+  //return sum;
 };
 
-const multiply = function() {
+const multiply = function () {
 
 };
 
-const power = function() {
-	
+const power = function () {
+
 };
 
-const factorial = function() {
-	
+const factorial = function () {
+
 };
 
 // Do not edit below this line
